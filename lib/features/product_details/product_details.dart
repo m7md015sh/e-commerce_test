46d02951571b610a4603380dd5e59439ec384960
m7md015sh <m7md015shokry@gmail.com>
2024-08-11 00:20:09 +0300

    done product info in product details screen

--- lib/features/product_details/product_details.dart
+++ lib/features/product_details/product_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test1/features/product_details/details_cubit/details_cubit.dart';
 import 'package:test1/features/product_details/details_cubit/details_state.dart';
+import 'package:test1/features/product_details/wedgits/product_info_widget.dart';
 
 class ProductDetailsPage extends StatelessWidget {
   @override
@@ -21,66 +22,79 @@ class ProductDetailsPage extends StatelessWidget {
               currentIndex = state.currentIndex;
             }
 
-            return Column(
-              children: [
-                SizedBox(
-                  height: 400.0,
-                  child: Stack(
-                    children: [
-                      PageView.builder(
-                        itemCount: imageUrls.length,
-                        controller: PageController(viewportFraction: 0.9),
-                        onPageChanged: (int index) {
-                          context.read<ProductDetailsCubit>().changeImage(index);
-                        },
-                        itemBuilder: (_, i) {
-                          return Transform.scale(
-                            scale: i == currentIndex ? 1 : 0.9,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 1.0),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Image.network(
-                                  imageUrls[i],
-                                  fit: BoxFit.fill,
-                                  width: double.infinity - 20,
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 400.0,
+                    child: Stack(
+                      children: [
+                        PageView.builder(
+                          itemCount: imageUrls.length,
+                          controller: PageController(viewportFraction: 0.9),
+                          onPageChanged: (int index) {
+                            context
+                                .read<ProductDetailsCubit>()
+                                .changeImage(index);
+                          },
+                          itemBuilder: (_, i) {
+                            return Transform.scale(
+                              scale: i == currentIndex ? 1 : 0.9,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 1.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Image.network(
+                                    imageUrls[i],
+                                    fit: BoxFit.fill,
+                                    width: double.infinity - 20,
+                                  ),
                                 ),
                               ),
+                            );
+                          },
+                        ),
+                        Positioned(
+                          bottom: 20.0,
+                          right: 40.0,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12.0),
                             ),
-                          );
-                        },
-                      ),
-                      Positioned(
-                        bottom: 20.0,
-                        right: 40.0,
-                        child: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          child: Row(
-                            children: imageUrls.asMap().entries.map((entry) {
-                              return Container(
-                                width: 8.0,
-                                height: 8.0,
-                                margin: const EdgeInsets.symmetric(horizontal: 2.0),
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: currentIndex == entry.key
-                                      ? const Color.fromRGBO(0, 0, 0, 0.9)
-                                      : const Color.fromRGBO(0, 0, 0, 0.4),
-                                ),
-                              );
-                            }).toList(),
+                            child: Row(
+                              children: imageUrls.asMap().entries.map((entry) {
+                                return Container(
+                                  width: 8.0,
+                                  height: 8.0,
+                                  margin:
+                                      const EdgeInsets.symmetric(horizontal: 2.0),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: currentIndex == entry.key
+                                        ? const Color.fromRGBO(0, 0, 0, 0.9)
+                                        : const Color.fromRGBO(0, 0, 0, 0.4),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                // Add other product details below the image carousel
-              ],
+                  const ProductInfoWidget(
+                      brand: 'Calvin Klein',
+                      title: 'Calvin Klein Monogram Logo Crewneck T-Shirt - Men',
+                      availability: 'Available',
+                      rating: 5,
+                      reviewsCount: 4,
+                      description: 'Crafted from soft cotton for an effortless feel, this Calvin Klein t-shirt features an oversized monogram logo for an iconic, retro-inspired look. Detailed with a crewneck, a straight hem and twin-needle topstitching in a wide array of colors.',
+                  ),
+                ],
+              ),
             );
           },
         ),
